f4d71a2212a78ffb0d5ca308c71c2685e7d8b089
Chameleon Cloud User <cc@ch-cascade.novalocal>
2022-07-27 03:56:59 +0000

    spraylist benchmark script modified

--- run_throughput.sh
+++ run_throughput.sh
@@ -11,20 +11,20 @@ fi
 
 bin=bin/spray
 
-#num_procs=(1 2 4 8 16 32 40 64 80)
-num_procs=(8 16)
+num_procs=(1 8 16 24 32)
+#num_procs=(8 16)
 #num_procs=(80)
 
-echo "#procs,spray,lotan_shavit,linden,skip"
+echo "#procs,spray" #,lotan_shavit,linden,skip
 
 for i in "${num_procs[@]}"
 do
-  spray[$i]=`$bin -l -i 1000000 -u 100 -d 1000 -n $i | grep $var | grep '(?<= )[0-9]+\.?[0-9]+' -Po`
-  lotan_shavit[$i]=`$bin -p -i 1000000 -u 100 -d 1000 -n $i | grep $var | grep '(?<= )[0-9]+\.?[0-9]+' -Po`
-  linden[$i]=`$bin -L -i 1000000 -u 100 -d 1000 -n $i | grep $var | grep '(?<= )[0-9]+\.?[0-9]+' -Po`
-  skip[$i]=`$bin -i 1000000 -u 100 -d 1000 -n $i | grep $var | grep '(?<= )[0-9]+\.?[0-9]+' -Po`
+  spray[$i]=`taskset -c 0-31 $bin -l -i 1000000 -u 100 -d 5000 -n $i | grep $var | grep '(?<= )[0-9]+\.?[0-9]+' -Po`
+  # lotan_shavit[$i]=`$bin -p -i 1000000 -u 100 -d 50000 -n $i | grep $var | grep '(?<= )[0-9]+\.?[0-9]+' -Po`
+  # linden[$i]=`$bin -L -i 1000000 -u 100 -d 50000 -n $i | grep $var | grep '(?<= )[0-9]+\.?[0-9]+' -Po`
+  # skip[$i]=`$bin -i 1000000 -u 100 -d 50000 -n $i | grep $var | grep '(?<= )[0-9]+\.?[0-9]+' -Po`
 
-  echo $i,${spray[$i]},${lotan_shavit[$i]},${linden[$i]},${skip[$i]}
+  echo $i,${spray[$i]}#,${lotan_shavit[$i]},${linden[$i]},${skip[$i]}
 done
 
 
